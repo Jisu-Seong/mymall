5da321de6eed5fde2b6acb04c69fac0cc66cd4ae
Jisu-Seong <sjangela0615@gmail.com>
2024-01-13 13:32:20 +0900

    front token, login 완료

--- mall/src/slices/loginSlice.js
+++ mall/src/slices/loginSlice.js
@@ -1,8 +1,20 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { loginPost } from "../api/memberApi";
+import { getCookie, setCookie, removeCookie } from "../util/cookieUtil";
 
 const initState = {
   email: "",
+};
+
+const loadMemberCookie = () => {
+  // 쿠키에서 로그인 정보 로딩
+  const memberInfo = getCookie("member");
+
+  // 닉네임 처리
+  if (memberInfo && memberInfo.nickname) {
+    memberInfo.nickname = decodeURIComponent(memberInfo.nickname);
+  }
+  return memberInfo;
 };
 
 export const loginPostAsync = createAsyncThunk("loginPostAsync", (param) => {
@@ -11,7 +23,7 @@ export const loginPostAsync = createAsyncThunk("loginPostAsync", (param) => {
 
 const loginSlice = createSlice({
   name: "LoginSlice",
-  initialState: initState,
+  initialState: loadMemberCookie() || initState,
   reducers: {
     login: (state, action) => {
       console.log("login...");
@@ -21,6 +33,7 @@ const loginSlice = createSlice({
     },
     logout: (state, action) => {
       console.log("logout...");
+      removeCookie("member");
       return { ...initState };
     },
   },
@@ -29,6 +42,11 @@ const loginSlice = createSlice({
       .addCase(loginPostAsync.fulfilled, (state, action) => {
         console.log("fullfilled");
         const payload = action.payload;
+
+        // 정상적인 로그인시에만 저장
+        if (!payload.error) {
+          setCookie("member", JSON.stringify(payload), 1); // 1일
+        }
         return payload;
       })
       .addCase(loginPostAsync.pending, (state, action) => {
